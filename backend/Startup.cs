ff896b37d36cd0ee8d523629ceef8786147113c8
SelfRef <git@aperte.dev>
2020-09-30 08:40:34 +0200

    Fix seed

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -36,10 +36,11 @@ namespace ToothSoupAPI
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, Database context)
 		{
 			if (env.IsDevelopment())
 			{
+				context.Database.EnsureCreated();
 				app.UseDeveloperExceptionPage();
 			}
 
